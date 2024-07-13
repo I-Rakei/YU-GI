82ba14da858cb8f7e5c139f0c5b71827342c26eb
IsmealRakei <ismaelrakei41@gamil.com>
2024-07-13 10:14:32 +0200

    added the profile image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { ref, getDownloadURL } from "firebase/storage";
+import { onAuthStateChanged } from "firebase/auth";
+import { storage, auth } from "../firebase";
 
 const Navbar = () => {
+  const [profilePicture, setProfilePicture] = useState(null);
+  const navigate = useNavigate();
+
+  const loadProfilePicture = async (uid) => {
+    try {
+      const url = await getDownloadURL(ref(storage, `profile_pictures/${uid}`));
+      setProfilePicture(url);
+    } catch (error) {
+      console.error("Error loading profile picture: ", error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        loadProfilePicture(currentUser.uid);
+      } else {
+        navigate("/login");
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [navigate]);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div className="container">
@@ -38,6 +66,20 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        {profilePicture ? (
+          <img
+            src={profilePicture}
+            alt="Profile"
+            className="img-thumbnail bg-dark"
+            style={{
+              width: "30px",
+              height: "30px",
+              objectFit: "cover",
+            }}
+          />
+        ) : (
+          "No profile picture"
+        )}
       </div>
     </nav>
   );
